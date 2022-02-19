5bc89ee704156d161e7135b3738aaa46ec68eb5c
neilswart3 <neilswart3@gmail.com>
2022-02-19 10:00:35 +0200

    Add header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import * as Styled from './styles'
+
+interface Props {
+
+}
+
+const Header: React.FC<Props> = (props) => {
+  return <Styled.Header>Header component</Styled.Header>
+}
+
+export default Header
