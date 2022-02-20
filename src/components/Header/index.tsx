05b464a4e9b695fea8abb64c555376ed4bdf2c16
neilswart3 <neilswart3@gmail.com>
2022-02-20 07:58:58 +0200

    Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,20 @@
+import { Typography } from '@mui/material'
 import * as Styled from './styles'
 
 interface Props {
-
+  title: string
 }
 
-const Header: React.FC<Props> = (props) => {
-  return <Styled.Header>Header component</Styled.Header>
+const Header: React.FC<Props> = ({ title }) => {
+  return (
+    <Styled.HeaderWrap>
+      <Styled.Header position='static' color='transparent' enableColorOnDark>
+        <Typography variant='h3' component='h1'>
+          {title}
+        </Typography>
+      </Styled.Header>
+    </Styled.HeaderWrap>
+  )
 }
 
 export default Header
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,3 +1,15 @@
-import styled from '@emotion/styled'
+import styled from '@emotion/styled/macro'
+import { AppBar } from '@mui/material'
+import { rgba } from 'polished'
+import { colors } from 'src/lib/theme'
 
-export const Header = styled.div``
+export const HeaderWrap = styled.div`
+  background-color: ${rgba(colors.white, 0.3)};
+  border-radius: 0 0 3rem 3rem;
+  backdrop-filter: blur(1rem);
+`
+
+export const Header = styled(AppBar)`
+  padding: 2.5rem 2rem;
+  box-shadow: none;
+`
